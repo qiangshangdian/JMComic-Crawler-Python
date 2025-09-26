9b9a92d16c0fc94428aaf98305cb2c2b28aa833e
qiangshangdian <qiangshangdian@gmail.com>
2025-09-27 01:53:41 +0800

    Update workflow_download.py

--- usage/workflow_download.py
+++ usage/workflow_download.py
@@ -3,11 +3,11 @@ from jmcomic.cl import JmcomicUI
 
 # 下方填入你要下载的本子的id，一行一个，每行的首尾可以有空白字符
 jm_albums = '''
-jm1213260
-jm179294
-jm1026680
-jm389982
-jm614147
+jm123762
+jm349762
+jm1214551
+jm324930
+jm192416
 '''
 # 单独下载章节
 jm_photos = '''
